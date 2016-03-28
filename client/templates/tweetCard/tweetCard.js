d0df9b4d14c302f3de903a6f80e5d06afdaab2dd
NoahVlncrt <noah0000games@gmail.com>
2016-03-28 11:16:01 -0400

    vote checking: checks to see if user is in alreadyVoted

--- client/templates/tweetCard/tweetCard.js
+++ client/templates/tweetCard/tweetCard.js
@@ -1,7 +1,12 @@
 Template.tweetCard.events({
   'click .uparrow': function(){
-    Tweets.update(this._id, {$inc:{score:1}});
-    Tweets.update({_id: this._id}, {$push:{alreadyVoted: Meteor.userId()}});
+  	var tweetArray = Tweets.findOne({_id: this._id});
+  	console.log(tweetArray.alreadyVoted.includes(Meteor.userId()))
+    if(tweetArray.alreadyVoted.includes(Meteor.userId()) === true){
+      console.log("YOU DID IT, GOOD JOB NOAH!")
+    } else {
+      console.log("WHY DID YOU FAIL YOU SHIT????")
+    }
   },
   'click .downarrow': function(){
     Tweets.update(this._id, {$inc:{score:-1}});
@@ -11,5 +16,8 @@ Template.tweetCard.events({
 Template.tweetCard.helpers({
   equals: function(v1,v2){
     return (v1 === v2);
+  },
+  voted: function() {
+    return Tweets.find({alreadyVoted: this.userId});
   }
 });
